fa0560106b9776b8c36b26cfa485aa3c0bbf5d29
nahuelmf <nahuelmf.sl@gmail.com>
2023-01-18 18:57:05 -0300

    ajustes

---
+++ Clase26/src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    mongoDB: {
+        url: 'mongodb+srv://nahuelmf:nawueh0731@cluster0.irezwt9.mongodb.net/test',
+        options: {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            //useCreateIndex: true,
+        }
+    }
+}
+module.exports = config
